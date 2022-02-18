d2e81f90db75bf889c175b12fd405510137aa2c9
Sumoshi Tanaka <sumoshi.tanaka@gmail.com>
2022-02-18 19:52:16 +0700

    Updated with Sumokoin fork rules and transfer utils

--- src/monero_transfer_utils.cpp
+++ src/monero_transfer_utils.cpp
@@ -71,26 +71,25 @@ bool monero_transfer_utils::is_tx_spendtime_unlocked(
 	network_type nettype
 ) {
 	if(unlock_time < CRYPTONOTE_MAX_BLOCK_NUMBER)
-	{
-		//interpret as block index
-		if(blockchain_size-1 + CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS >= unlock_time)
-			return true;
-		else
-			return false;
-	}else
-	{
-		//interpret as time
-		uint64_t current_time = static_cast<uint64_t>(time(NULL));
-		// XXX: this needs to be fast, so we'd need to get the starting heights
-		// from the daemon to be correct once voting kicks in
-		uint64_t v2height = nettype == TESTNET ? 624634 : nettype == STAGENET ? (uint64_t)-1/*TODO*/ : 1009827;
-		uint64_t leeway = block_height < v2height ? CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V1 : CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V2;
-		if(current_time + leeway >= unlock_time)
-			return true;
-		else
-			return false;
-	}
-	return false;
+    {
+      //interpret as block index
+      if(blockchain_size-1 + CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS >= unlock_time)
+        return true;
+      else
+        return false;
+    }else
+    {
+      //interpret as time
+      uint64_t current_time = static_cast<uint64_t>(time(NULL));
+      // XXX: this needs to be fast, so we'd need to get the starting heights
+      // from the daemon to be correct once voting kicks in
+      uint64_t leeway = CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS;
+      if(current_time + leeway >= unlock_time)
+        return true;
+      else
+        return false;
+    }
+    return false;
 }
 //
 namespace {
